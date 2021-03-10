0ef9352a7101e951b17f8c70ed1c6047d106b773
aknarayan <akshay.narayan5599@gmail.com>
2021-03-10 22:36:55 +0100

    Draw simple triangle

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -39,8 +39,9 @@ int main(void)
 		glfwTerminate();
 		return -1;
 	}
+	glfwMakeContextCurrent(window);
 
-	glfwMakeContextCurrent(window); // Initialize GLEW
+	// Initialize GLEW
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -51,6 +52,23 @@ int main(void)
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	// VertexArrayObject, VAO
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Array of 3 vectors, representing 3 vertices
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	GLuint vertexBuffer; // vertex buffer ID
+	glGenBuffers(1, &vertexBuffer); // generate 1 buffer, put resulting identifier in vertexBuffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW); // give vertices to OpenGL
+
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
@@ -58,7 +76,21 @@ int main(void)
 		// Clear the screen - can cause flickering
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw nothing, see you in tutorial 2!
+		// 1st attribute buffer: vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glVertexAttribPointer(
+			0,			// attribute 0 - no particular reason, but must match layout in shader
+			3,			// size
+			GL_FLOAT,	// type
+			GL_FALSE,	// normalized?
+			0,			// stride
+			(void*)0	// array buffer offset
+		);
+
+		// Draw triangle
+		glDrawArrays(GL_TRIANGLES, 0, 3); // starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
