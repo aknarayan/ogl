011cb7cdbdb43a0593a74e3f0b40c1c95c37cb1e
aknarayan <akshay.narayan5599@gmail.com>
2021-03-16 22:26:45 +0100

    Introduce model, view, and projection matrices

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -13,12 +13,15 @@
 
 // maths library
 #include <glm/glm.hpp>
+
+// matrix transform functions
+#include <glm/gtc/matrix_transform.hpp>
+
 using namespace glm;
 
 int main(void)
 {
 	// initialise GLFW
-	glewExperimental = true; // needed for core profile
 	if (!glfwInit())
 	{
 		fprintf(stderr, "Failed to initialize GLFW\n");
@@ -27,7 +30,6 @@ int main(void)
 	}
 
 	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
-	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // OpenGL 3.3
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // to make MacOS happy; should not be needed
@@ -44,6 +46,7 @@ int main(void)
 	glfwMakeContextCurrent(window);
 
 	// initialize GLEW
+	glewExperimental = true; // needed for core profile
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -54,10 +57,38 @@ int main(void)
 	// ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	// dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
 	// VertexArrayObject, VAO
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
+
+	// create and compile GLSL program from shaders
+	GLuint programID = LoadShaders("simplevertexshader.glsl", "simplefragmentshader.glsl");
+
+	// get handle for MVP uniform, only done during initialisation
+	GLuint matrixID = glGetUniformLocation(programID, "MVP");
+
+	// perspective projection matrix: 45° fov, 4:3 aspect ratio, near clipping plane 0.1, far clipping plane 100.0
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f/3.0f, 0.1f, 100.0f);
+	
+	// orthographic projection matrix
+	//glm::mat4 projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f);
+
+	// camera (view) matrix
+	glm::mat4 view = glm::lookAt(
+		glm::vec3(4, 3, 3), // camera is at (4, 3, 3) in world space
+		glm::vec3(0, 0, 0), // camera looks at origin
+		glm::vec3(0, 1, 0)  // up vector is (0, 1, 0)
+	);
+
+	// model matrix (identity, i.e. model will be at origin)
+	glm::mat4 model = glm::mat4(1.0f);
+
+	// model-view-projection matrix
+	glm::mat4 mvp = projection * view * model;
 
 	// array of 3 vectors, representing 3 vertices
 	static const GLfloat g_vertex_buffer_data[] = {
@@ -71,15 +102,16 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW); // give vertices to OpenGL
 
-	// dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-	// create and compile GLSL program from shaders
-	GLuint programID = LoadShaders("simplevertexshader.glsl", "simplefragmentshader.glsl");
-
 	do {
 		// clear the screen - can cause flickering
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// use shader
+		glUseProgram(programID);
+
+		// send transformation to currently bound shader, in the "MVP" uniform
+		// done in the loop as each model will have a different M matrix and therefore a different MVP matrix
+		glUniformMatrix4fv(matrixID, 1, GL_FALSE, &mvp[0][0]);
 
 		// 1st attribute buffer: vertices
 		glEnableVertexAttribArray(0);
@@ -93,9 +125,6 @@ int main(void)
 			(void*)0	// array buffer offset
 		);
 
-		// use shader
-		glUseProgram(programID);
-
 		// draw triangle
 		glDrawArrays(GL_TRIANGLES, 0, 3); // starting from vertex 0; 3 vertices total -> 1 triangle
 		glDisableVertexAttribArray(0);
@@ -106,6 +135,11 @@ int main(void)
 	} // check if ESC key was pressed or the window was closed
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0);
+
+	// cleanup VBO and shader
+	glDeleteBuffers(1, &vertexBuffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// close OpenGL window and terminate GLFW
 	glfwTerminate();
